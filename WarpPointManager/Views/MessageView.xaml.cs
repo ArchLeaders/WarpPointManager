df3e27accfc6f0969d2f4daaadeb13fb60e2f49c
ArchLeaders <archparliament@gmail.com>
2022-06-14 20:28:14 -0700

    System dialog view/model

---
+++ WarpPointManager/Views/MessageView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+using WarpPointManager.ViewModels;
+
+namespace WarpPointManager.Views
+{
+    /// <summary>
+    /// Interaction logic for MessageView.xaml
+    /// </summary>
+    public partial class MessageView : Window
+    {
+        public MessageView()
+        {
+            InitializeComponent();
+            DataContext = new MessageViewModel("No details were provided.");
+            SourceInitialized += (s, a) => {
+                Dispatcher.Invoke(InvalidateVisual, DispatcherPriority.Input);
+            };
+
+            btnOk.Focus();
+        }
+    }
+}
