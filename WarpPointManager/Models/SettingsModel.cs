ca0caa36391838faa7712ea46ad71db76eb8ae2e
ArchLeaders <archparliament@gmail.com>
2022-06-14 20:27:55 -0700

    Settings view/model and serializer object

---
+++ WarpPointManager/Models/SettingsModel.cs
@@ -0,0 +1,18 @@
+﻿using WarpPointManager.ViewModels;
+
+namespace WarpPointManager.Models
+{
+    public class SettingsModel
+    {
+        public string BaseDump { get; set; }
+        public string UpdateDump { get; set; }
+        public string DlcDump { get; set; }
+
+        public SettingsModel(SettingsViewModel svm)
+        {
+            BaseDump = svm.BaseDump;
+            UpdateDump = svm.UpdateDump;
+            DlcDump = svm.DlcDump;
+        }
+    }
+}
---
+++ WarpPointManager/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Stylet;
+using System;
+using System.IO;
+using System.Text.Json;
+using WarpPointManager.Models;
+
+namespace WarpPointManager.ViewModels
+{
+    public class SettingsViewModel : Screen
+    {
+        private string _baseDump = "";
+        public string BaseDump {
+            get => _baseDump;
+            set => SetAndNotify(ref _baseDump, value);
+        }
+
+        private string _updateDump = "";
+        public string UpdateDump {
+            get => _updateDump;
+            set => SetAndNotify(ref _updateDump, value);
+        }
+
+        private string _dlcDump = "";
+        public string DlcDump {
+            get => _dlcDump;
+            set => SetAndNotify(ref _dlcDump, value);
+        }
+
+        public void Browse(string mode)
+        {
+            System.Windows.Forms.FolderBrowserDialog openFld = new();
+            
+            if (openFld.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+
+                // Base
+                if (mode == "base") {
+                    BaseDump = openFld.SelectedPath;
+                }
+
+                // Update
+                if (mode == "update") {
+                    BaseDump = openFld.SelectedPath;
+                }
+
+                // DLC
+                if (mode == "dlc") {
+                    BaseDump = openFld.SelectedPath;
+                }
+            }
+        }
+
+        public void Save()
+        {
+            string path = $"{Environment.GetEnvironmentVariable("LOCALAPPDATA")}\\WarpPointManager";
+
+            Directory.CreateDirectory(path);
+            File.WriteAllText($"{path}\\config.json", JsonSerializer.Serialize(new SettingsModel(this)));
+
+            WindowManager.Show("Settings saved succefully.");
+        }
+
+        private IWindowManager? WindowManager { get; set; }
+        public SettingsViewModel(IWindowManager? windowManager)
+        {
+            WindowManager = windowManager;
+        }
+    }
+}
